905e2c997de90e4da7e9fc5de38e1ba03ce31641
Ilya Litovchik <litovchic@gmail.com>
2018-11-07 14:07:56 +0300

    text

--- laba2/index.js
+++
@@ -1,9 +0,0 @@
-menu.onclick=function Navbar(){
-    var x=document.getElementById('mynavbar')
-    if(x.className==='navbar'){
-        x.className+=' responsive';
-    }else
-    {
-        x.className='navbar';
-    }
-}
